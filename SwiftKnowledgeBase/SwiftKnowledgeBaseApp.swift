f275240f205f8b876f0a24c343d9d4213735585c
Denis IOS Developer <denis.poddubnyy.111@gmail.com>
2024-06-05 17:08:22 +0300

    Initial Commit

---
+++ SwiftKnowledgeBase/SwiftKnowledgeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftKnowledgeBaseApp.swift
+//  SwiftKnowledgeBase
+//
+//  Created by User on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftKnowledgeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
